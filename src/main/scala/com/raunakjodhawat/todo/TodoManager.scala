be8dc6238f240f4a829ab3f247c0cc4c6e8559f6
Raunak Jodhawat <raunakjodhawat@gmail.com>
2024-10-18 10:46:48 +0530

    Introduce todo manager

--- src/main/scala/com/raunakjodhawat/todo/TodoManager.scala
+++ src/main/scala/com/raunakjodhawat/todo/TodoManager.scala
@@ -1,5 +1,25 @@
 package com.raunakjodhawat.todo
 
-object TodoManager {
+import com.raunakjodhawat.filehandling.FileManagerConfig.{
+  createFileIfDoesNotExist,
+  fileLocation
+}
+import zio.stream.{ZPipeline, ZStream}
+import zio.{Chunk, ZIO}
 
+import java.io.File
+import java.time.LocalDate
+
+object TodoManager {
+  def getTodo(
+      profileName: Option[String],
+      date: Option[LocalDate]
+  ): ZIO[Any, Throwable, Chunk[String]] = {
+    createFileIfDoesNotExist *>
+      ZStream
+        .fromFile(new File(fileLocation))
+        .via(ZPipeline.utf8Decode)
+        .via(ZPipeline.splitLines)
+        .runCollect
+  }
 }
